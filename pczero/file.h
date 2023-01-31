578c3bab548a48f513b10a5440beb90f2a7887ee
calint <calin.tenitchi@gmail.com>
2023-01-31 08:36:57 +0100

    .

--- pczero/file.h
+++ pczero/file.h
@@ -2,16 +2,16 @@ typedef char* Addr;
 class Ref{
 	Addr addr;
 public:
-	inline Ref(Addr addr):addr{addr}{}
+	inline Ref(Addr addr0):addr{addr0}{}
 	inline Addr get_addr()const{return addr;}
 };
 
 typedef int Size;
 
-class File:Ref{
+class File:public Ref{
 	Size size;
 public:
-	inline File(Addr addr,Size size):Ref{addr},size{size}{}
+	inline File(Addr addr0,Size size0):Ref{addr0},size{size0}{}
 	void to(File file);
 	void to(File file,Size nbytes);
 	inline Size get_size()const{return size;}
